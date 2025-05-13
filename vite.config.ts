575c447cff1b1bab86c1c4a0120d92c1b79539a1
Taharayyan <65994521+Taharayyan@users.noreply.github.com>
2025-05-14 00:59:33 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/easyridev3/', // <-- This must match your repo name!
+  base: '/', // <-- This must match your repo name!
 });
